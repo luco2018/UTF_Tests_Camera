92a3376bab0bd410e19086bc20018904b57b5448
Andre McGrail <andremcgrail@Andres-MacBook-Pro.local>
2017-11-07 16:32:57 +0000

    Massive cleanup to the camera tests

---
+++ Scenes/CameraSettings/HDR/BasicPost.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BasicPost : MonoBehaviour
+{
+
+    private Material material;
+
+    // Creates a private material used to the effect
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/HalfBrightness"));
+    }
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
---
+++ Scripts/CullingCheck.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CullingCheck : MonoBehaviour {
+
+    public Text _text;
+
+    public Renderer[] rends;
+	public Renderer[] visibleRends;
+    public bool passed;
+
+    IEnumerator Start () {
+        yield return new WaitForEndOfFrame();
+
+        passed = true;
+        foreach(Renderer rend in rends)
+		{
+			if(rend.isVisible)
+                passed = false;
+        }
+		foreach (Renderer visibleRend in visibleRends)
+        {
+            if (!visibleRend.isVisible)
+                passed = false;
+        }
+
+		if(passed)
+		{
+            //do passed
+            _text.color = Color.green;
+            _text.text = "PASS";
+        }
+		else
+		{
+            //do failed
+            _text.color = Color.red;
+            _text.text = "FAIL";
+        }
+	}
+}
