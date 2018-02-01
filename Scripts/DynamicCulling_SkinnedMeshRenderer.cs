504d1a6382c3adf84ece5d3058ead0d29f7c1626
MingWai <cmwdes@gmail.com>
2018-02-01 16:52:33 +0000

    added camera test for checking dynamic culling for skinned mesh renderer

---
+++ Scripts/DynamicCulling_SkinnedMeshRenderer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class DynamicCulling_SkinnedMeshRenderer : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform spawnPoints;
+    private int count = 0;
+    private Vector3 offset = Vector3.zero;
+    public int noOfitem = 10;
+    public float spacing = 0.2f;
+    public int maxNoOfSpheres = 100;
+
+
+    void Start ()
+    {
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if(count < maxNoOfSpheres)
+        {
+            if (count % noOfitem == 0)
+            {
+                offset.z += spacing;
+                offset.x = 0;
+            }
+            else
+            {
+                offset.x += (count - Mathf.Floor(count / noOfitem) * noOfitem) * spacing * 0.5f;
+            }
+            Instantiate(enemy, spawnPoints.position + offset, spawnPoints.rotation);
+            count++;
+        }
+
+    }
+}
